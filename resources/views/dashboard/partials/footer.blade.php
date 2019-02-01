ef5730a6d85360159dc7379a063509603e03c8a9
peekinternational <peekinternational@gmail.com>
2019-02-01 14:50:30 -0800

    mmmmm

--- resources/views/dashboard/partials/footer.blade.php
+++ resources/views/dashboard/partials/footer.blade.php
@@ -2,6 +2,6 @@
     <div class="pull-right hidden-xs">
         <b>Version</b> 1.0
     </div>
-    <strong>Copyright &copy; 2018 <a href="{{ url('/') }}">{{ env('APP_NAME','Islamic Wall design') }}</a>.</strong> All rights
+    <strong>Copyright &copy; 2018 <a href="{{ url('/') }}">{{ env('APP_NAME','Pekk let') }}</a>.</strong> All rights
     reserved.
 </footer>
